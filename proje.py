8f15162bdf14fb4584fdc65edfa17fd403506af6
elifsevuuk <elif.elifse00@gmail.com>
2025-04-06 22:25:15 +0300

    Update proje.py

---
+++ proje.py
@@ -0,0 +1,14 @@
+print("Hello World!")  
+isim = ("Elif")
+print(isim)
+isim = input("Adınızı giriniz: ")
+print("Merhaba," + isim + " ! ")
+yas = int(input("Yaşınızı girin: "))
+if yas < 18:
+    print("Henüz reşit değilsiniz!")
+else:
+    print("Reşitsiniz")
+    print("Hoşgeldiniz")
+    print("Genç yetişkinsiniz")
+sehir = input(" Hangi şehirde yaşıyorsun:")  
+print("Harika, orası güzel bir şehir!")
